ccec43386dce0352bded62ff07f79c9a743dd026
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-09-03 18:39:30 +0100

    Update main.py

--- Programming Project/main.py
+++ Programming Project/main.py
@@ -1,1 +1,23 @@
+#Main game loop
 
+#Event handler 
+running = True 
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False 
+      
+  keys = pygame.key.get_pressed()
+
+  #Horizontal player movement
+  if keys[pygame.K_LEFT]:
+    playerSpeedX = -PLAYER_SPEED
+  elif keys[pygame.K_RIGHT]:
+    playerSpeedX = PLAYER_SPEED
+  else:
+    playerSpeedX = 0
+
+  #Vertical player movement
+  if keys[pygame.K_SPACE] and onGround:
+    playerSpeedY = jumpPower
+    onGround = False
