f56ea610c58ce8a11583fae0f90c25de261ad560
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-09-03 16:32:59 +0100

    Update constants.py

--- Programming Project/constants.py
+++ Programming Project/constants.py
@@ -1,1 +1,16 @@
-#Glob
+#Global Constants
+
+#Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+#Player dimensions and characteristics 
+PLAYER_WIDTH = 64
+PLAYER_HEIGHT = 64
+PLAYER_SPEED = 5
+
+#Level dimensions
+GROUND_HEIGHT = 50
+
+#Colour
+WHITE = (255, 255, 255)
