e94be3bfab7b330b085757029602060651e42ee5
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-09-03 21:04:08 +0100

    Update sprite.py

--- Programming Project/sprite.py
+++ Programming Project/sprite.py
@@ -1,11 +1,11 @@
-#Player setup
+#Sprite setup
 
-#Player dimensions
+#Sprite dimensions
 player = pygame.Rect(50, SCREEN_HEIGHT - GROUND_HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
-#Player Characteristics 
-playerSpeedX = 0
-playerSpeedY = 0
+#Sprite Characteristics 
+spriteSpeedX = 0
+spriteSpeedY = 0
 gravity = 0.5
 jumpPower  = -10
 onGround = False 
