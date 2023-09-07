a9be6dfdec9a756facd622b293faa668f797339e
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-09-07 19:01:04 +0100

    Create hud.py

---
+++ Programming Project/hud.py
@@ -0,0 +1,35 @@
+#Set HUD font
+font = pygame.font.Font(None, 36)
+
+#set the initial score and lives tally
+score = 0
+lives = 3
+
+#Function to update the score
+def updateScore(points):
+  global score
+  score += points
+
+#Function to update the lives
+def updateLives(life):
+  global lives
+  lives += life
+
+#Render the score
+scoreText = font.render("Score: " + str(score), True, (255, 255, 255))
+game_display.blit(text, (10,10))
+
+#Render the lives
+livesText = font.render("Lives: ", + str(lives), True, (255,255,255))
+screen.blit
+
+#Update display
+pygame.display.flip()
+
+#Control frame rate
+clock.tick(60)
+
+#Quit pygame
+pygame.quit()
+
+
