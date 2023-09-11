1940e1ac734970b15a9af63d42d8c39bebcdc19f
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-09-11 21:34:46 +0100

    Update Button.py

--- Programming Project/Button.py
+++ Programming Project/Button.py
@@ -1,1 +1,19 @@
+#Define button class
+def Button:
+  def __init__(self, x, y, button_width, button_height, text):
+    self.rect = pygame.Rect(x, y, button_width, button_height)
+    self.text = text
 
+  def draw(self):
+    pygame.draw.rect(screen, WHITE, self.rect)
+    font = pygame.font.Font(None, 36)
+    text =font.render(self.text, True, BLACK)
+    text_Rect = text.get_rect(center = self.rect.center)
+    screen.blit(text, text_Rect)
+
+  def isClicked(self, pos):
+    return self.rect.collidepoint(pos)
+
+#Create the buttons 
+play_button = Button(300, 200, 200, 50, "Play")
+quit_button = Button(300, 300, 200, 50, "Quit")
