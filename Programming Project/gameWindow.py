995b98157bffc598d93635ef103cb7b88d06b00c
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-09-11 21:08:25 +0100

    Update gameWindow.py

--- Programming Project/gameWindow.py
+++ Programming Project/gameWindow.py
@@ -4,11 +4,20 @@ import sys
 #initialise pygame
 pygame.init()
 
+#Event handler 
+running = True 
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False 
+
 #Creating game window 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Jario")
 
-
+#Blit the screen
+screen.blit()
+pygame.display.flip()
 
 
 
