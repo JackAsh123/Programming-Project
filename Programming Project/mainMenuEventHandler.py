1ddec95abb874e11afe88a99de85e7a254dc4852
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-09-11 21:36:59 +0100

    Update mainMenuEventHandler.py

--- Programming Project/mainMenuEventHandler.py
+++ Programming Project/mainMenuEventHandler.py
@@ -1,1 +1,16 @@
+# Main game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if play_button.is_clicked(pos):
+                print("Play button clicked!")
+                # Add code here to start the game
+            elif quit_button.is_clicked(pos):
+                pygame.quit()
+                sys.exit()
+
 
