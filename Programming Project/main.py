6760499d06d7285bc12d85283b4e62b69e6cfca1
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-11-12 21:37:27 +0000

    Update main.py

--- Programming Project/main.py
+++ Programming Project/main.py
@@ -2,15 +2,13 @@
 import pygame
 from pygame.locals import *
 
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Jario")
 
 
-#Event handler 
-running = True 
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False 
-      
   keys = pygame.key.get_pressed()
 
   #Horizontal sprite movement
