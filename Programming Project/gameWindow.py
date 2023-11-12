671e2b8287de5fea416b4e6879d3a1e21b402c7a
JackAsh123 <139239584+JackAsh123@users.noreply.github.com>
2023-11-12 21:38:51 +0000

    Update gameWindow.py

--- Programming Project/gameWindow.py
+++ Programming Project/gameWindow.py
@@ -1,3 +1,50 @@
+ keys = pygame.key.get_pressed()
+
+  #Horizontal sprite movement
+  if keys[pygame.K_LEFT]:
+    spriteSpeedX = -SPRITE_SPEED
+  elif keys[pygame.K_RIGHT]:
+    spriteSpeedX = SPRITE_SPEED
+  else:
+    spriteSpeedX = 0
+
+  #Vertical sprite movement
+  if keys[pygame.K_SPACE] and onGround:
+    spriteSpeedY = jumpPower
+    onGround = False
+
+  #Gravity
+  if not onGround:
+    spriteSpeedY += gravity 
+
+  #Updating sprite positions
+  sprite.x += spriteSpeedX
+  sprite.y += spriteSpeedY
+
+  #Keep sprite in the game window
+  if sprite.left < 0:
+    sprite.left = 0 
+  if sprite.right > SCREEN_WIDTH:
+    sprite.right = SCREEN_WIDTH
+  if sprite.top < 0:
+    sprite.top = 0 
+  if sprite.bottom > SCREEN_HEIGHT - GROUND_HEIGHT:
+    sprite.bottom = SCREEN_HEIGHT - GROUND_HEIGHT
+    onGround = True 
+
+  #Clearing the screen
+  screen.fill(WHITE)
+
+  #Drawing the ground
+  pygme.draw.rect(screen, (0, 255, 0), (0, SCREEN_HEIGHT - GROUND_HEIGHT, SCREEN_WIDTH, GROUND_HEIGHT)
+
+  #Drawing the sprite 
+  pygame.draw.rect(screen, (0, 255, 0), sprite)
+
+  #pygame.display.update()
+
+
+
 
 
 
